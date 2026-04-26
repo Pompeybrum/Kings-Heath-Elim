e8c0b9054676c64ed3932c48a5c2f7fe4139a540
Pompeybrum <jonpompey@googlemail.com>
2026-04-26 20:11:35 +0100

    Add files via upload

--- kings-heath-elim/js/components.js
+++ kings-heath-elim/js/components.js
@@ -51,9 +51,9 @@ function injectNav(activePage) {
           </div>
         </div>
         <button class="nav-cta" onclick="location.href='${prefix}pages/contact.html'">Plan Your Visit</button>
-        <button class="nav-hamburger" id="nav-hamburger" aria-label="Menu">
+        <label class="nav-hamburger" onclick="(function(){var d=document.getElementById('nav-drawer');d.classList.toggle('open');})()" aria-label="Menu">
           <span></span><span></span><span></span>
-        </button>
+        </label>
       </div>
       <div class="nav-drawer" id="nav-drawer">
         <a href="${prefix}index.html">Home</a>
